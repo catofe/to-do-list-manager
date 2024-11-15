0422c5cdd5b98259b8d07939409526ca46dfb847
mirigrips <catherinemanayaga22@gmail.com>
2024-11-15 14:04:30 +0800

    added edit function

--- main.py
+++ main.py
@@ -15,20 +15,19 @@ class Task:
     def __init__(self):
         # constructor code here
         print("Object Created")
+        self.title = ""
 
     # task destructor
     def __del__(self):
         # destructor code here
         print("Object Destroyed")
 
-    def edit(self):
-        print("Editing Task")
+    def edit(self, new_name):
+        print(f"Editing Task: {self.title} to {new_name}")
+        self.title = new_name
+        print("Task name updated successfully.\n")
         # edit code here
-
-    def delete(self):
-        print("Deleting Task")
-        # delete code here
-
+        
     def mark_complete(self):
         print("Task Complete")
         # mark_complete code here
