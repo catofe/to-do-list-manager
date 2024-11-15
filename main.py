84507ece5e4a5efc6d1cb1965767de5a8b9c76ad
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-15 13:45:28 +0800

    created boilerplate code for todo list manager

---
+++ main.py
@@ -0,0 +1,22 @@
+class Tasks:
+    # tasks constructor
+    def __init__(self):
+        # constructor code here
+        print("Object Created")
+
+    # tasks destructor
+    def __del__(self):
+        # destructor code here
+        print("Object Destroyed")
+
+
+class Task:
+    # task constructor
+    def __init__(self):
+        # constructor code here
+        print("Object Created")
+
+    # task destructor
+    def __del__(self):
+        # destructor code here
+        print("Object Destroyed")
